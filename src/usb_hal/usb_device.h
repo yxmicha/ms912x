45585e7be852a5dcac498d11d356f982eddef0de
Micha <220009159+yxmicha@users.noreply.github.com>
2026-05-14 15:56:00 +0000

    drm/ms912x: add HAL adaptor layer and device ID declarations
    Defines the msdisp_hal_id / msdisp_hal_dev / msdisp_hal_funcs
    structs that decouple the DRM driver from specific USB chip HALs,
    and declares msdisp_hal_find_dev() for device matching.
    Ported from MacroSilicon vendor driver source.
    
    Signed-off-by: Micha <220009159+yxmicha@users.noreply.github.com>

---
+++ src/usb_hal/usb_device.h
@@ -0,0 +1,32 @@
+/* Copyright (C) 2023 MacroSilicon Technology Co., Ltd.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * usb_device.h -- Drm driver for MacroSilicon chip 913x and 912x
+ */
+
+#ifndef __MSDISP_MS9132_H__
+#define __MSDISP_MS9132_H__
+
+#include "hal_adaptor.h"
+
+#define MSDISP_913X_VENDOR	0x345f
+#define MSDISP_9132_PRODUCT	0x9132
+#define MSDISP_9133_PRODUCT	0x9133
+#define MSDISP_9135_PRODUCT	0x9135
+
+extern const struct msdisp_hal_id ms9132_id;
+extern const struct msdisp_hal_id ms9133_id;
+extern const struct msdisp_hal_id ms9135_id;
+extern struct msdisp_hal_dev ms9132_dev;
+extern struct msdisp_hal_dev ms9133_dev;
+extern struct msdisp_hal_dev ms9135_dev;
+
+#endif
