4bfad4f626691c42dd53a353458f97febb252ed6
Micha <220009159+yxmicha@users.noreply.github.com>
2026-05-14 14:32:00 +0000

    drm/ms912x: add DRM and USB-HAL event structures
    Declares the tagged usb_hal_event union (enable/disable/keepalive)
    that the DRM driver posts to the USB HAL work queue.
    Ported from MacroSilicon vendor driver source.
    
    Signed-off-by: Micha <220009159+yxmicha@users.noreply.github.com>

---
+++ src/drm/msdisp_drm_event.h
@@ -0,0 +1,48 @@
+/* Copyright (C) 2023 MacroSilicon Technology Co., Ltd.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * msdisp_drm_event.h -- Drm driver for MacroSilicon chip 913x and 912x
+ */
+
+#ifndef __MSDISP_DRM_EVENT_H__
+#define __MSDISP_DRM_EVENT_H__
+
+#include <drm/drm_file.h>
+
+/* drm request custom event type starts from 0x80000000 */
+#define MSDISP_EVENT_TYPE_BASE		0x90000000
+#define MSDISP_EVENT_TYPE_ENABLE	(MSDISP_EVENT_TYPE_BASE + 1)
+#define MSDISP_EVENT_TYPE_DISABLE	(MSDISP_EVENT_TYPE_BASE + 2)
+#define MSDISP_EVENT_TYPE_UPDATE	(MSDISP_EVENT_TYPE_BASE + 3)
+
+struct msdisp_drm_event {
+	struct drm_event base;
+	union {
+		struct {
+			unsigned int format;
+			unsigned short width;
+			unsigned short height;
+			unsigned char rate;
+			unsigned char resv[3];
+		} enable;
+
+		struct {
+			unsigned int len;
+			unsigned int resv[2];
+		} update;
+
+		struct {
+			unsigned int resv[3];
+		} disable;
+	} para;
+};
+
+#endif
